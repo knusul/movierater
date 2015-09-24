90474eee34e822e0c53a909cf25cdb16e234e133
Magdalena Nieznalska <magdalena.nieznalska@gmail.com>
2015-09-24 12:50:16 +0200

    Empty title and description are valid for a movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should allow empty title and description"do
+    expect(build(:movie, title: "", description: "")).to be_valid
+  end
 end
