ac64e32ce347922dd130f0bfa870750d18171540
Magdalena Nieznalska <magdalena.nieznalska@gmail.com>
2015-09-24 12:59:57 +0200

    Remove unused rating controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :movies
-  resources :ratings, only: :update
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
